040f00cf0aa0fe3b8dc50c4605112e7146a7c1ca
Efraim Flashner <efraim@flashner.co.il>
2021-07-27 07:23:40 +0000

    gnu: %boot3-inputs: Add missing input.
    * gnu/packages/commencement.scm (%boot3-inputs): Add gcc:lib.

--- gnu/packages/commencement.scm
+++ gnu/packages/commencement.scm
@@ -3586,6 +3586,7 @@ exec ~a/bin/~a-~a -B~a/lib -Wl,-dynamic-linker -Wl,~a/~a \"$@\"~%"
 (define (%boot3-inputs)
   ;; 4th stage inputs.
   `(("gcc" ,gcc-final)
+    ("gcc:lib" ,gcc-final "lib")
     ("ld-wrapper" ,ld-wrapper-boot3)
     ,@(alist-delete "gcc" (%boot2-inputs))))
 
